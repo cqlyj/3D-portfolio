01c38b55aff090f2130e83a5bffba9abac4082fb
cqlyj <luoyingjie0721@gmail.com>
2024-10-14 10:50:03 +0800

    Add Contact section to main App component

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,121 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_rskc88l",
+        "template_l9duvzu",
+        {
+          from_name: form.name,
+          to_name: "Luo Yingjie",
+          from_email: form.email,
+          to_email: "luoyingjie0721@gmail.com",
+          message: form.message,
+        },
+        "gZdveH5y6suI8vH39"
+      );
+      setLoading(false);
+      alert("Message sent successfully!");
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+      alert("Failed to send message. Something went wrong.");
+    }
+  };
+
+  return (
+    <section className="c-space my-20">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img
+          src="/assets/terminal.png"
+          alt="terminal background"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container">
+          <h3 className="head-text">Let&apos;s talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you are looking to build a new dapp, need a smart contract
+            audit, or just want to chat about blockchain technology, I&apos;m
+            here to help.
+          </p>
+
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="Bob"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="Bob@gmail.com"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Hi, I wanna give you a job ..."
+              />
+            </label>
+
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send Message"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
