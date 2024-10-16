b14105d14a9842ea77992f9cbb877da66d706f34
cqlyj <luoyingjie0721@gmail.com>
2024-10-17 02:58:35 +0800

    Add Experience component to main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import About from "./sections/About";
 // import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
-// import Experience from "./sections/Experience";
+import Experience from "./sections/Experience";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
@@ -15,7 +15,7 @@ const App = () => {
       <About></About>
       <Projects></Projects>
       {/* <Clients></Clients> */}
-      {/* <Experience></Experience> */}
+      <Experience></Experience>
       <Contact></Contact>
       <Footer></Footer>
     </main>
