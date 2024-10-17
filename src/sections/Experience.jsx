fd69f55b54403a34d9f782c32be63111a4bb1f10
cqlyj <luoyingjie0721@gmail.com>
2024-10-17 11:56:07 +0800

    change the intensity of ambientLight

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -17,7 +17,7 @@ const WorkExperience = () => {
         <div className="work-container">
           <div className="work-canvas">
             <Canvas>
-              <ambientLight intensity={7} />
+              <ambientLight intensity={3} />
               <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
               <directionalLight position={[10, 10, 10]} intensity={1} />
               <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
